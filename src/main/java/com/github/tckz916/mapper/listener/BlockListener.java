54d6bec757d64ebe5ae5a248e27d271aeb27c8f0
tckz916 <tachikaze0916@gmail.com>
2015-08-16 22:16:18 +0900

    Mapper

--- src/main/java/com/github/tckz916/mapper/listener/BlockListener.java
+++ src/main/java/com/github/tckz916/mapper/listener/BlockListener.java
@@ -1,6 +1,6 @@
 package com.github.tckz916.mapper.listener;
 
-import com.github.tckz916.mapper.util.Util;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -13,7 +13,7 @@ public class BlockListener implements Listener {
     @EventHandler
     public void onSign(SignChangeEvent event) {
         for (int i = 0; i <= 3; i++) {
-            event.setLine(i, Util.coloring(event.getLine(i)));
+            event.setLine(i, ChatColor.translateAlternateColorCodes('&', event.getLine(i)));
         }
     }
 
